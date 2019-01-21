1e33cbff4424111d8737f1970b0dbf43ffe950a9
lieby weintraub <liebahinda@gmail.com>
2019-01-21 04:06:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end 
 
 def introduction_with_laungauge(name, language)
-  puts "Hi, my name is #{name} and I am learning to progra in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
