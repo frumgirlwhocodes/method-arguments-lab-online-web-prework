65342ac03950507263885c9f801cacbb356e87de
lieby weintraub <liebahinda@gmail.com>
2019-01-21 04:05:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-def introduction_with_laungauge
+def introduction_with_laungauge(name, language)
+  puts "Hi, my name is #{name} and I am learning to progra in #{language}"
